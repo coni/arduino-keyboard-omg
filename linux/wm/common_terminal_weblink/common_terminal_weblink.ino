ca208a3d1b754c6fc4672d7747cf7fd1009bfeaa
coni <46821134+coni@users.noreply.github.com>
2023-09-16 09:38:24 +0000

    hehe

---
+++ linux/wm/common_terminal_weblink/common_terminal_weblink.ino
@@ -0,0 +1,26 @@
+#include <Keyboard.h>
+#include <KeyboardLayout.h>
+#include <Keyboard_it_IT.h>
+#include <Keyboard_da_DK.h>
+#include <Keyboard_fr_FR.h>
+#include <Keyboard_de_DE.h>
+#include <Keyboard_es_ES.h>
+#include <Keyboard_sv_SE.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Keyboard.begin();
+  delay(600);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.println("");
+  Keyboard.releaseAll();
+  delay(400);
+  Keyboard.println("xdg-open https://www.youtube.com/watch?v=lxdvyPzOv1w & disown");
+  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.print("C");
+  Keyboard.releaseAll();
+}
+
+void loop() {
+}
